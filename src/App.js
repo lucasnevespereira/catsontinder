38b12b46f6760be7bc23908152cfe08f1c267ae0
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-08-03 14:19:59 +0200

    added header w/ material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build catsOnTinder 😻</h1>
+      {/* <h1>Let's build catsOnTinder 😻</h1> */}
       {/* HEADER */}
+      <Header />
       {/* App Cards */}
       {/* Buttons */}
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './Header.css';
+import cat from './cat.svg';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 
-function Header() {
-  return <div></div>;
-}
+const Header = () => {
+  return (
+    <div className="header">
+      <PersonIcon className="header__icon" fontSize="large" />
+      {/* <h2>Logo</h2> */}
+      <img className="header__logo" src={cat} alt="cat logo" />
+      <ForumIcon className="header__icon" fontSize="large" />
+    </div>
+  );
+};
 
 export default Header;
